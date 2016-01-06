365a0ad23ee75d0f111740bb78fcc59de653d691
Alexander <a.starwars.d@gmail.com>
2016-01-06 22:51:24 +0200

    Set ups angularjs

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,10 +1,33 @@
-var app = angular.module('RealtimeForum', ['ngRoute']);
+var app = angular.module('RealtimeForum', ['ngRoute', 'ngCookies']);
 
-app.controller('IndexCtrl', ['$scope', function($scope) {
+app.controller('IndexCtrl', ['$scope', function ($scope) {
     $scope.test = 'Hello, World'
 }]);
 
-app.config(['$routeProvider', function($routeProvider) {
+app.factory('$common', ['$cookies', function($cookies) {
+    return {
+        loggedIn: function() {
+            return !!($cookies.get('user-id') && $cookies.get('token'));
+        }
+    }
+}]);
+
+app.controller('HeaderCtrl', ['$scope', '$common', function($scope, $common) {
+    $scope.loggedIn = $common.loggedIn();
+    $scope.logIn = function() {
+        alert('Log In');
+    };
+}]);
+
+app.directive('appHeader', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/javascript/templates/app-header.html',
+        controller: 'HeaderCtrl'
+    }
+});
+
+app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider.when('/', {
         templateUrl: '/javascript/templates/index.html',
         controller: 'IndexCtrl'
